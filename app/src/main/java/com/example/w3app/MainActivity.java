8be9ae8fd9c93993c334ea2aaf556dca668c41eb
Aaron-SCC <aaron.lewis@seattlecentral.edu>
2019-04-16 20:19:13 -0700

    add red background color

--- app/src/main/java/com/example/w3app/MainActivity.java
+++ app/src/main/java/com/example/w3app/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.w3app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "We are in the OnCreate method of MainActivity");
+
     }
 }
